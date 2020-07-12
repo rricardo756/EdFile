135fad6c0e734b66d39a50306df35a3f4a8eef6a
rricardo756 <67746121+rricardo756@users.noreply.github.com>
2020-07-12 10:34:28 -0400

    Update Student_Desc_Stats.sql

--- Student_Desc_Stats.sql
+++ Student_Desc_Stats.sql
@@ -1,11 +1,11 @@
 SELECT 
 Student_ID,
 Student_Name,
-Round(Avg(Price),0)              AS Mean_Score,
-Max(Price)                       AS High_Score,
-Min(Price)                       AS Low_Score,
-100*(Max(Price)/Min(Price) - 1)  AS Pct_Diff_Hi_Lo,
-Count(Price)                     AS Score_Count
+Round(Avg(Score),0)              AS Mean_Score,
+Max(Score)                       AS High_Score,
+Min(Score)                       AS Low_Score,
+100*(Max(Score)/Min(Score) - 1)  AS Pct_Diff_Hi_Lo,
+Count(Score)                     AS Score_Count
 
 FROM 
 [Student_Table]
